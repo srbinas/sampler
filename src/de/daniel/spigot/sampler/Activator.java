1736672326c577394b3626863dd6db758d0fe1d0
srbinas <daniel.nikolov.hh@gmail.com>
2019-01-18 09:56:43 +0100

    Update Activator.java

--- src/de/daniel/spigot/sampler/Activator.java
+++ src/de/daniel/spigot/sampler/Activator.java
@@ -9,12 +9,12 @@ public class Activator extends JavaPlugin {
        
     @Override
     public void onEnable() {  
-        System.out.println("Sampler läuft!");
+        System.out.println("Sampler is running!");
     }
 
     @Override
     public void onDisable() {
-        System.out.println("Sampler endet!");
+        System.out.println("Sampler ends!");
     }
     
     public boolean onCommand(final CommandSender sender, Command command, String label, String[] args) {
@@ -23,13 +23,13 @@ public class Activator extends JavaPlugin {
         
         if (sender.hasPermission("sampler") || sender.hasPermission("samplerop")) {
             if (currentCommand.equals("hallo")) {
-                sender.sendMessage(ChatColor.GREEN + "Funzt doch perfekt, " + sender.getName() + "!");
+                sender.sendMessage(ChatColor.GREEN + "Hello world, " + sender.getName() + "!");
             }
         }
         
         if (sender.hasPermission("samplerop")) {
             if (currentCommand.equals("hallo2")) {
-                sender.sendMessage(ChatColor.DARK_RED + "NOCH DAZU: Du bist op, " + sender.getName() + "!");
+                sender.sendMessage(ChatColor.DARK_RED + "You are an OP user, " + sender.getName() + "!");
             }
         }
         
